7110ef7ae6dc2e22d0a8353c239f189d66eb440c
Larkin Heintzman <larkin.heintzman515@gmail.com>
2024-11-21 20:20:55 -0500

    encrypted version

--- games/BabyBuilds/ModularMachines_v1.2.service.worker.js
+++ games/BabyBuilds/ModularMachines_v1.2.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1732231803|1144064884';
+const CACHE_VERSION = '1732238255|7596142107';
 /** @type {string} */
 const CACHE_PREFIX = 'ModularMachines-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
